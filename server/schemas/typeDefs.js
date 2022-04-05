a95cd6a404d7d64ae5a20d68ec200c5b409282e2
Austin Miller <austin.miller1424@gmail.com>
2022-04-05 13:32:50 -0500

    populated typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,11 +1,36 @@
 // import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs
+// STILL MISSING : image and link permas in Mutation.saveBook input type there as well, als oimage and link in booktype 
 const typeDefs = gql`
 type Query {
-    helloWorld: String
+    me: User
   }
+type Mutation {
+  login(email: String!, username: String!):Auth
+  addUser (email: String!, username: String!, password:String!): Auth
+  saveBook(authors: Array!, description: String!, title: String!, bookId: String!): User, input
+  removeBook:(bookId: String!): User
+}
+type User{
+  id: ID
+  username: String
+  email: String 
+  bookcount: Int
+  savedBooks: [book]
+}
+type Book{
+  bookdId:
+  authors: Array
+  description:String
+  title: String
+  image: 
+  link:
+}
+type Auth{
+  token:
+  user: User
+}
 `;
 
 // export the typeDefs
